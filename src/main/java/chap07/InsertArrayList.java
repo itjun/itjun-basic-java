ee0084f76231105f156fe668ce0ec01313027374
黄荣君 <l1091462907@gmail.com>
2018-05-29 17:43:54 +0800

    增加 slf4j 日志

--- src/main/java/chap07/InsertArrayList.java
+++ src/main/java/chap07/InsertArrayList.java
@@ -1,5 +1,8 @@
 package chap07;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 
@@ -7,15 +10,12 @@ import java.util.LinkedList;
  * Created by iCrazyTeam on 2017/1/17.
  */
 public class InsertArrayList {
+    private static final Logger log = LoggerFactory.getLogger(InsertArrayList.class);
+
     public static void main(String[] args) {
-        //step1
         step1();
 
-        //step2
         step2();
-
-        //step3
-        step3();
     }
 
     private static void step1() {
@@ -24,24 +24,16 @@ public class InsertArrayList {
         for (int i = 0; i < 10000000; i++) {
             list1.add(i);
         }
-        System.out.println("spend " + (System.currentTimeMillis() - start) + " ms");
+        log.info("ArrayList spend {} ms", (System.currentTimeMillis() - start));
     }
+
 
     private static void step2() {
-        ArrayList<Integer> list2 = new ArrayList<Integer>();
-        long start = System.currentTimeMillis();
-        for (int i = 10000000; i < 20000000; i++) {
-            list2.add(i);
-        }
-        System.out.println("spend " + (System.currentTimeMillis() - start) + " ms");
-    }
-
-    private static void step3() {
         LinkedList<Integer> list3 = new LinkedList<>();
         long start = System.currentTimeMillis();
         for (int i = 0; i < 10000000; i++) {
             list3.add(i);
         }
-        System.out.println("spend " + (System.currentTimeMillis() - start) + " ms");
+        log.info("LinkedList spend {} ms", (System.currentTimeMillis() - start));
     }
 }
