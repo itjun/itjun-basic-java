146ffbb1013701ca1d53b1a76a8dd8dfd031990f
黄荣君 <l1091462907@gmail.com>
2019-11-25 19:24:42 +0800

    增加gson解析map

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 [![Build Status](https://secure.travis-ci.org/itjun/java-examples.png)](https://travis-ci.org/itjun/java-examples)
 
-该学习项目全程使用 `IntelliJ IDEA` 进行学习和开发，以上说明！
+1. 该学习项目全程使用 `IntelliJ IDEA` 进行学习和开发，以上说明！
+2. 为了尽量确保代码的清晰和可执行性，尽量将每个范例直接放置到一个类的main方法中运行。
+3. 代码即注释，相关的说明和注解直接在代码中。
--- src/main/java/io/itjun/examples/json/gson/deserialization/GsonToMap.java
+++ src/main/java/io/itjun/examples/json/gson/deserialization/GsonToMap.java
@@ -1,15 +1,19 @@
-package io.itjun.examples.json.gson.serialization;
+package io.itjun.examples.json.gson.deserialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import lombok.extern.slf4j.Slf4j;
 
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Created by iCrazyTeam on 2017/5/14.
+ * json 反序列化成Map
  */
-public class MapToJson {
+@Slf4j
+public class GsonToMap {
 
     public static void main(String[] args) {
         Map<String, String> items = new HashMap<>();
@@ -20,6 +24,15 @@ public class MapToJson {
 
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         String json = gson.toJson(items);
-        System.out.println(json);
+        log.info("原始数据 {}", json);
+
+        Type type = new TypeToken<Map<String, String>>() {
+        }.getType();
+        Map<String, String> results = gson.fromJson(json, type);
+        results.forEach((k, v) -> {
+            log.info("{} {}", k, v);
+        });
+
     }
+
 }
