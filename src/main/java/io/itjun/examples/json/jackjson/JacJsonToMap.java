36c1be5ea70765f0f20ad583f1376e17d49aabed
黄荣君 <l1091462907@gmail.com>
2019-11-26 10:09:22 +0800

    Create JacJsonToMap.java

---
+++ src/main/java/io/itjun/examples/json/jackjson/JacJsonToMap.java
@@ -0,0 +1,34 @@
+package io.itjun.examples.json.jackjson;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class JacJsonToMap {
+
+    public static void main(String[] args) throws JsonProcessingException {
+        Map<String, String> items = new HashMap<>();
+        items.put("C0001", "Apple");
+        items.put("C0002", "Google");
+        items.put("C0003", "Facebook");
+        items.put("C0004", "Tencent");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(items);
+        log.info("{}", json);
+
+        TypeFactory typeFactory = mapper.getTypeFactory();
+        MapType mapType = typeFactory.constructMapType(HashMap.class, String.class, String.class);
+        HashMap<String, String> map = mapper.readValue(json, mapType);
+        map.forEach((k, v) -> {
+            log.info("{} {}", k, v);
+        });
+    }
+
+}
