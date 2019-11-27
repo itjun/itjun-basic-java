e58f34ace5bcfdb093fccabc516ea1eb12e32250
黄荣君 <l1091462907@gmail.com>
2019-11-27 23:59:51 +0800

    增加JackJson解析数据对象的范例

--- src/main/java/io/itjun/examples/json/jackjson/JackJsonToObject.java
+++ src/main/java/io/itjun/examples/json/jackjson/JackJsonToObject.java
@@ -1,6 +1,7 @@
 package io.itjun.examples.json.jackjson;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,6 +13,23 @@ import java.util.Arrays;
 public class JackJsonToObject {
 
     public static void main(String[] args) throws ParseException, JsonProcessingException {
+        String json = "{\"resultcode\":\"200\",\"reason\":\"查询物流信息成功\",\"result\":{\"company\":\"EMS\",\"com\":\"ems\",\"no\":\"1186465887499\",\"status\":\"1\",\"list\":[{\"datetime\":\"2016-06-15 21:44:04\",\"remark\":\"离开郴州市 发往长沙市【郴州市】\",\"zone\":\"\"},{\"datetime\":\"2016-06-20 17:55:00\",\"remark\":\"投递并签收，签收人：单位收发章 *【毕节地区】\",\"zone\":\"\"}]},\"error_code\":0}";
+        ObjectMapper mapper = new ObjectMapper();
+
+        Logistics logistic = mapper.readValue(json, new TypeReference<Logistics>() {
+        });
+        log.info("{}", logistic.getReason());
+
+        LogisticsInfo info = logistic.getResult();
+        info.getList().forEach((obj) -> {
+            log.info("{}", obj.getDatetime());
+            log.info("{}", obj.getRemark());
+            log.info("{}", obj.getZone());
+        });
+
+    }
+
+    private static void objToJson() throws ParseException, JsonProcessingException {
         SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
         ActorJackson rudyYoungblood = new ActorJackson("nm2199632", sdf.parse("21-09-1982"),
                 Arrays.asList("Apocalypto", "Beatdown", "Wind Walkers"));
---
+++ src/main/java/io/itjun/examples/json/jackjson/LogisticsTrace.java
@@ -0,0 +1,20 @@
+package io.itjun.examples.json.jackjson;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class LogisticsTrace {
+
+    @Setter
+    @Getter
+    private String datetime;
+
+    @Setter
+    @Getter
+    private String remark;
+
+    @Setter
+    @Getter
+    private String zone;
+
+}
