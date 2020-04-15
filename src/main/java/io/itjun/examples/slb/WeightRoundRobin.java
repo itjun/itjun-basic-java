0d36d740960b49de925ef83be15fd1336335c83b
黄荣君 <l1091462907@gmail.com>
2020-04-15 10:25:32 +0800

    增加负载均衡轮询模拟

---
+++ src/main/java/io/itjun/examples/slb/WeightRoundRobin.java
@@ -0,0 +1,45 @@
+package io.itjun.examples.slb;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class WeightRoundRobin {
+
+    private static Integer pos;
+
+    /**
+     * 根据权重进行轮询
+     */
+    public static String getServer() {
+        // 重建一个Map，避免服务器的上下线导致的并发问题
+        Map<String, Integer> serverMap = new HashMap<>(IPMap.items);
+
+        // 取得Ip地址List
+        Set<String> keySet = serverMap.keySet();
+        Iterator<String> iterator = keySet.iterator();
+
+        List<String> serverList = new ArrayList<>();
+        while (iterator.hasNext()) {
+            String server = iterator.next();
+            int weight = serverMap.get(server);
+            for (int i = 0; i < weight; i++) {
+                serverList.add(server);
+            }
+        }
+
+        String server = null;
+        synchronized (pos) {
+            if (pos > keySet.size()) {
+                pos = 0;
+            }
+            server = serverList.get(pos);
+            pos++;
+        }
+
+        return server;
+    }
+}
