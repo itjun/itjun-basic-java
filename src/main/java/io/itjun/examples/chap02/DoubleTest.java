53fa7f608cf9dd1c886265b31e47cb2c1f0a2b90
黄荣君 <l1091462907@gmail.com>
2020-05-21 23:52:37 +0800

    Update DoubleTest.java

--- src/main/java/io/itjun/examples/chap02/DoubleTest.java
+++ src/main/java/io/itjun/examples/chap02/DoubleTest.java
@@ -27,10 +27,15 @@ public class DoubleTest {
         BigDecimal d2 = new BigDecimal(Double.toString(0.3));
         System.out.println(d1.add(d2).doubleValue());
         // System.out.println(d1.divide(d2));// 出现无限循环小数点报错
-        System.out.println(d1.divide(d2, 2, RoundingMode.HALF_UP).doubleValue());
+        System.out.println(d1.divide(d2, 2, RoundingMode.HALF_EVEN).doubleValue()); // 银行家舍入法
 
         System.out.println(1.1 + 1.2);  //输出：2.3
         System.out.println(0.1f + 0.2f);//输出：0.3
+
+        // 四舍五入测试
+        System.out.println("12.5的四舍五入值：" + Math.round(12.5));// 12.5的四舍五入值：13
+        System.out.println("-12.5的四舍五入值：" + Math.round(-12.5));// -12.5的四舍五入值：-12
+
     }
 
 }
