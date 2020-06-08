e00e6c194d1d1bdf5a8a279bce06798374ae9464
黄荣君 <l1091462907@gmail.com>
2020-06-08 19:50:02 +0800

    add Java8 LocalTime

---
+++ src/main/java/io/itjun/examples/time/LocalTimeTest.java
@@ -0,0 +1,19 @@
+package io.itjun.examples.time;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class LocalTimeTest {
+
+    public static void main(String[] args) {
+        LocalDateTime n1 = LocalDateTime.now();
+        System.out.println(n1);
+        System.out.println(n1.getHour());
+        System.out.println(n1.getMinute());
+
+        // 生成指定的时分
+        LocalTime now = LocalTime.of(1, 3);
+        System.out.println(now.toString());
+    }
+
+}
