5e281176c3fbb65b14ab71b35839844550421985
黄荣君 <l1091462907@gmail.com>
2020-08-10 17:41:22 +0800

    Create TowSum001.java

---
+++ src/main/java/io/itjun/examples/leecode/TowSum001.java
@@ -0,0 +1,34 @@
+package io.itjun.examples.leecode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TowSum001 {
+
+    public int[] twoSum(int[] nums, int target) {
+        int result[] = new int[2];
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int[] twoSumHash(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
+
+
+
