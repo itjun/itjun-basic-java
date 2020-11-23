646451b14cbcbc185d6a6722ff55dbdf8975de0e
黄荣君 <l1091462907@gmail.com>
2020-11-23 23:41:53 +0800

    增加字符串变量

---
+++ src/main/java/io/itjun/examples/chap02/MathCalc.java
@@ -0,0 +1,14 @@
+package io.itjun.examples.chap02;
+
+public class MathCalc {
+
+    public static void main(String[] args) {
+        System.out.println("5 + 6");
+        System.out.println(5 + 6);
+        System.out.println(5 - 6);
+        System.out.println(5 * 6);
+        System.out.println(5 / 6.0d);
+        System.out.println((1 + 2 + 3) * 4 / 5.0);
+    }
+
+}
