74232c6d08959848fab41a444b23884aa760019b
IT君 <l1091462907@gmail.com>
2021-05-06 23:53:27 +0800

    Update TestAtomic.java

--- src/main/java/io/itjun/examples/thread/TestAtomic.java
+++ src/main/java/io/itjun/examples/thread/TestAtomic.java
@@ -1,14 +1,14 @@
 package io.itjun.examples.thread;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-
 @Slf4j
-public class Test implements Runnable {
+public class TestAtomic implements Runnable {
 
     @Getter
     private static AtomicLong sum = new AtomicLong();
@@ -16,7 +16,7 @@ public class Test implements Runnable {
     private int x;
     private int y;
 
-    public Test(int x, int y) {
+    public TestAtomic(int x, int y) {
         this.x = x;
         this.y = y;
     }
@@ -32,7 +32,7 @@ public class Test implements Runnable {
         long start = System.currentTimeMillis();
         List<Thread> list = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
-            list.add(new Thread(new Test(1, 1000000)));
+            list.add(new Thread(new TestAtomic(1, 1000000)));
         }
         list.forEach(t -> {
             t.start();
@@ -44,7 +44,7 @@ public class Test implements Runnable {
                 e.printStackTrace();
             }
         });
-        log.info("{}", Test.getSum());
+        log.info("{}", TestAtomic.getSum());
         log.info("{}", System.currentTimeMillis() - start);
     }
 }
