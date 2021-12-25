dd566ee08fa4fac8019bf1eaaab82e3101b79c49
IT君 <l1091462907@gmail.com>
2021-12-25 13:18:31 +0800

    增加map、set、list遍历对比

---
+++ src/test/java/io/itjun/examples/string/CollectionsLoopTest.java
@@ -0,0 +1,57 @@
+package io.itjun.examples.string;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 百万数据遍历，list、set、map 对比速度
+ */
+@Slf4j
+public class CollectionsLoopTest {
+
+    private static final int size = 1000000;
+
+    @Test
+    public void test_list() {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add("value" + i);
+        }
+        long start = System.nanoTime();
+        boolean result = list.contains("value" + (size - 1));
+        long end = System.nanoTime();
+        log.info("list执行时间 {}, {}", (end - start), result);
+    }
+
+    @Test
+    public void test_set() {
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            set.add("value" + i);
+        }
+        long start = System.nanoTime();
+        boolean result = set.contains("value" + (size - 1));
+        long end = System.nanoTime();
+        log.info("set执行时间 {}, {}", (end - start), result);
+    }
+
+    @Test
+    public void test_map() {
+        Map<String, String> sets = new HashMap();
+        for (int i = 0; i < size; i++) {
+            sets.put("key" + i, "value" + i);
+        }
+        long start = System.nanoTime();
+        boolean result = sets.containsKey("key" + (size - 1));
+        long end = System.nanoTime();
+        log.info("map执行时间 {}, {}", (end - start), result);
+    }
+
+}
