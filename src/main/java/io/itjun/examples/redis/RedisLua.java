9a44df52f67dabfaefec92d090ef58b350a95c72
HUANG Rongjun <l1091462907@gmail.com>
2022-04-23 22:15:27 +0800

    Update RedisLua.java

--- src/main/java/io/itjun/examples/redis/RedisLua.java
+++ src/main/java/io/itjun/examples/redis/RedisLua.java
@@ -7,11 +7,11 @@ public class RedisLua {
                 + "local idx=1;" + "local argVIdx=1;" + "for idx=1,keysLen,1 do " + "argVIdx=(idx-1)*2+1; "
                 + "redis.call('Set',KEYS[idx],ARGV[argVIdx],'EX',ARGV[argVIdx+1]);" + "end " + "return keysLen;";
 
-        try (Jedis jedis = JedisFactory.getJedis()) {
-            jedis.scriptLoad(LUA_SCRIPT_MSETEX);
-            jedis.evalsha(jedis.scriptLoad(LUA_SCRIPT_MSETEX), Arrays.asList("a1", "a2"),
-                    Arrays.asList("v1", "10", "v2", "20"));
-        }
+//        try (Jedis jedis = JedisFactory.getJedis()) {
+//            jedis.scriptLoad(LUA_SCRIPT_MSETEX);
+//            jedis.evalsha(jedis.scriptLoad(LUA_SCRIPT_MSETEX), Arrays.asList("a1", "a2"),
+//                    Arrays.asList("v1", "10", "v2", "20"));
+//        }
     }
 
 }
