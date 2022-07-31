0a3651a50dbff4822ea514c5124d330c374cfec9
HUANG Rongjun <l1091462907@gmail.com>
2022-07-31 15:21:05 +0800

    Collectors.toMap 转指定类型的Map

--- src/main/java/io/itjun/examples/lambda/LambdaList.java
+++ src/main/java/io/itjun/examples/lambda/LambdaList.java
@@ -3,7 +3,9 @@ package io.itjun.examples.lambda;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class LambdaList {
@@ -20,6 +22,10 @@ public class LambdaList {
         log.info("max {}", list.stream().mapToInt(Integer::parseInt).max().getAsInt());
         log.info("anyMatch {}", list.stream().mapToInt(Integer::parseInt).filter(k -> k > 1).anyMatch(k -> k == 5));
         list.stream().mapToInt(Integer::parseInt).filter(k -> k > 1).sorted().forEach(System.out::println);
+
+        // 转指定的 LinkedHashMap
+        LinkedHashMap<String, String> collect = list.stream().collect(Collectors.toMap(k -> k, k -> k, (o1, o2) -> o1, LinkedHashMap::new));
+        collect.forEach((key, value) -> log.info("{} {}", key, value));
     }
 
 }
