5d91c2c79b33e6c996c87d819b2c4985a833e887
HUANG Rongjun <l1091462907@gmail.com>
2022-09-11 23:42:41 +0800

    增加动态代理范例

--- src/main/java/io/itjun/examples/json/gson/serialization/Keynote.java
+++ src/main/java/io/itjun/examples/json/gson/serialization/Keynote.java
@@ -1,10 +1,21 @@
 package io.itjun.examples.json.gson.serialization;
 
+import com.google.gson.Gson;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Keynote {
 
+    private static final Map<String, Keynote> items = new HashMap<>();
+    static {
+        items.put("1",new Keynote("a1","b"));
+        items.put("2",new Keynote("a2","b"));
+        items.put("3",new Keynote("a3","b"));
+        items.put("4",new Keynote("a4","b"));
+    }
     @Getter
     @Setter
     private String value;
@@ -16,6 +27,10 @@ public class Keynote {
     public Keynote(String value, String color) {
         this.value = value;
         this.color = color;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Gson().toJson(items));
     }
 
 }
