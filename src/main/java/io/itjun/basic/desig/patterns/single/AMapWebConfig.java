32c5808175c652fcb7fe4a74b1789e190bdeb87b
HUANG Rongjun <l1091462907@gmail.com>
2023-11-11 01:16:35 +0800

    add

--- src/main/java/io/itjun/basic/desig/patterns/single/AMapWebConfig.java
+++ src/main/java/io/itjun/basic/desig/patterns/single/AMapWebConfig.java
@@ -8,27 +8,13 @@ import java.util.concurrent.atomic.AtomicInteger;
  * <p>
  * 用于坐标获取，静态地图等服务
  */
-public class AMapWebConfig {
+public enum AMapWebConfig {
+    instance;
     private final List<String> list;
     private static final AtomicInteger counter = new AtomicInteger();
-    private static volatile AMapWebConfig instance;
 
     private AMapWebConfig() {
         this.list = getList();
-    }
-
-    public static AMapWebConfig getInstance() {
-        if (instance == null) {
-            synchronized (AMapWebConfig.class) {
-                if (instance == null)
-                    instance = new AMapWebConfig();
-            }
-        }
-        return instance;
-    }
-
-    protected AMapWebConfig(List<String> list) {
-        this.list = list;
     }
 
     private List<String> getList() {
@@ -36,13 +22,13 @@ public class AMapWebConfig {
         return List.of(value.split(","));
     }
 
-    protected String getNext() {
+    private String getNext() {
         int index = counter.getAndIncrement() % list.size();
         return list.get(index);
     }
 
     public static String getKey() {
-        return AMapWebConfig.instance.getNext();
+        return instance.getNext();
     }
 
 }
--- src/main/java/io/itjun/basic/desig/patterns/single/AMapWebConfigTest.java
+++ src/main/java/io/itjun/basic/desig/patterns/single/AMapWebConfigTest.java
@@ -2,31 +2,31 @@ package io.itjun.basic.desig.patterns.single;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AMapWebConfigTest {
-    private static final int THREAD_COUNT = 3000;
+    private static final int THREAD_COUNT = 10000;
     private static final CountDownLatch startLatch = new CountDownLatch(1);
     private static final CountDownLatch finishLatch = new CountDownLatch(THREAD_COUNT);
 
     public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(THREAD_COUNT);
         Set<AMapWebConfig> set = new HashSet<>();
         for (int i = 0; i < THREAD_COUNT; i++) {
-            new Thread(() -> {
+            executorService.submit(() -> {
                 try {
                     startLatch.await(); // 等待所有线程都准备好
-                    AMapWebConfig config = AMapWebConfig.getInstance();
+                    AMapWebConfig config = AMapWebConfig.instance;
                     set.add(config);
-                    if (set.size() > 1) {
+                    if (set.size() > 1)
                         System.out.println("stop size " + set.size());
-                    }
                     finishLatch.countDown();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-            }).start();
+            });
         }
 
         // 让所有线程同时开始
@@ -34,6 +34,7 @@ public class AMapWebConfigTest {
 
         // 等待所有线程执行完毕
         finishLatch.await();
+        executorService.shutdownNow();
     }
 
 }
