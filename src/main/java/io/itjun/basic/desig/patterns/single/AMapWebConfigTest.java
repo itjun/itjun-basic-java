4e24bab13e4d80ae49be35585e03d250e1863924
HUANG Rongjun <l1091462907@gmail.com>
2023-12-05 21:06:00 +0800

    使用线程安全的计算器组件进行存值比较

--- src/main/java/io/itjun/basic/desig/patterns/single/AMapWebConfigTest.java
+++ src/main/java/io/itjun/basic/desig/patterns/single/AMapWebConfigTest.java
@@ -5,23 +5,22 @@ import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class AMapWebConfigTest {
     private static final int THREAD_COUNT = 3000;
     private static final CountDownLatch startLatch = new CountDownLatch(1);
     private static final CountDownLatch finishLatch = new CountDownLatch(THREAD_COUNT);
+    private static final AtomicReference<AMapWebConfig> last = new AtomicReference<>();
 
     public static void main(String[] args) throws InterruptedException {
-        Set<AMapWebConfig> set = new HashSet<>();
         for (int i = 0; i < THREAD_COUNT; i++) {
             new Thread(() -> {
                 try {
                     startLatch.await(); // 等待所有线程都准备好
                     AMapWebConfig config = AMapWebConfig.getInstance();
-                    set.add(config);
-                    if (set.size() > 1) {
-                        System.out.println("stop size " + set.size());
-                    }
+                    if (last.get() != config)
+                        last.set(config);
                     finishLatch.countDown();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
